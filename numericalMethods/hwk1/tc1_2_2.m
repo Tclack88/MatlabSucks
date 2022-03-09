1f1f27c973891c6417ef3926ec6056699a31fda2
Tclack88 <tclack@aol.com>
2022-03-10 09:27:23 +1100

    update and fixed convergence of negative root

--- numericalMethods/hwk1/tc1_2_2.m
+++ numericalMethods/hwk1/tc1_2_2.m
@@ -4,20 +4,36 @@
 % where f(x) = sin(2x) + (x^2)/3 -3 = 0
 % with fixed point iteration, we approximate the next term from the previous
 % x_i+1 = sqrt(3(3 - sin(2x_i)))
-fx = @(x) sin(pi/180*2*x) + x^2/3 - 3; %pi/180 to get redians
-gx = @(x) sqrt(3*(3 - sin(pi/180*2*x)));
+fx = @(x) sin(2*x) + x^2/3 - 3; %pi/180 to get redians
+gxp = @(x) sqrt(3*(3 - sin(2*x)));
+gxn = @(x) -sqrt(3*(3 - sin(2*x))); %sqrt gives + and - results
 
 guess1 = -2.5;
 guess2 = 3.1;
 
+% lower root (use gxn)
 h = 10; % for height. Initially relatively large number
 x = guess1;
 n = 0;
 while h >  .0001
-    x = gx(x);
+    x = gxn(x);
     actual = fx(x);
     h = abs(actual); 	% abs because if our guess can be + or -. We really
     n = n + 1;		% just care about the magnitude of the "height"
 end
 
-sprintf('A close approximation of x=%f achieved after %d iterations',x, n)
+sprintf('A close approximation of x=%f achieved for the lower root after %d iterations',x, n)
+
+
+% upper root (use gxp)
+h = 10; % for height. Initially relatively large number
+x = guess2;
+n = 0;
+while h >  .0001
+    x = gxp(x);
+    actual = fx(x);
+    h = abs(actual); 	% abs because if our guess can be + or -. We really
+    n = n + 1;		% just care about the magnitude of the "height"
+end
+
+sprintf('A close approximation of x=%f achieved for the upper root after %d iterations',x, n)

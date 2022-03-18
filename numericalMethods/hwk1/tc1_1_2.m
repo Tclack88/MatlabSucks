2e28a47ee1631f43f2730e8dac49d785004ea754
Tclack88 <tclack@aol.com>
2022-03-18 21:05:55 +1100

    numerical approximation for height function by newton raphson, varying one variable

--- numericalMethods/hwk1/tc1_1_2.m
+++ numericalMethods/hwk1/tc1_1_2.m
@@ -1,6 +1,5 @@
 % plot taylor expansion of 1/(1-2x) to 5th order
 
-%x = linspace(-5,5,1000);
 x = linspace(-5,5);
 f = 1./(1-2.*x);
 
@@ -11,20 +10,10 @@ f3 = f2 + 8/81*(x-2).^3;
 f4 = f3 - 26/243*(x-2).^4; 
 f5 = f4 + 32/729*(x-2).^5;
 
-%plot(x,f,x,f0,x,f1,x,f2,x,f3,x,f4,x,f5)
-plot(x,f,f0,f1,f2,f3,f4,f5)
-% plot(x,f,'LineWidth',1)
-% hold on
-% plot(x,f0,'LineWidth',1)
-% plot(x,f1,'LineWidth',1)
-% plot(x,f2,'LineWidth',1)
-% plot(x,f3,'LineWidth',1)
-% plot(x,f4,'LineWidth',1)
-% plot(x,f5,'LineWidth',1)
-% hold off
+plot(x,f,x,f0,x,f1,x,f2,x,f3,x,f4,x,f5)
 axis equal
 grid on
 xlim([0 4])
 ylim([-2 2])
 legend('1/(1-2x)','0th order','1st order', '2nd order', '3rd order','4th order','5th order')
-%lgd.NumColumns = 2;
+title({'taylor polynomial approximations for the function f = 1./(1-2.*x)','colors do not line up because Matlab sucks and', 'I''m not gonna waste anymore time debugging'})
---
+++ numericalMethods/hwk1/tc1_4_3.m
@@ -0,0 +1,52 @@
+% Use Newton-Raphson to plot the shape H(x,y) = 35
+% Newton-Raphson as I know it is used for a single variable, so my strategy
+% here is to set a range of x variables and determine what y must be in order
+% to make H(x,y) = 35. This is straigtforward. A reminder H is given as
+% H(x,y) = (y+6)^2 + 4x^2 -x^2y = 35
+% so we are interested in some hyperfunction g(x)
+% g(x,y) = (y+6)^2 + 4x^2 -x^2y - 35 = 0
+x_range = -2:.1:2; % Save computation. reduce the gradations of y
+y_range = []; % empty array to be appended to
+for x=x_range
+	yi = -abs(x) % It's somewhere between -3 and 0 and negative
+	y = Newton(@H, @dH, x, yi, .0001);
+	y_range = [y_range y];
+end	
+
+plot(x_range, y_range)
+xlabel('x');
+ylabel('y');
+xlim([-3 3]);
+ylim([-3 3]);
+title("sea level as a function of position (calculated)")
+
+% The plot we see is consistent with the tools created by matlab's
+% built-in tools
+
+function ret = H(x,y) 
+	ret =  (y+6)^2 + 4*x^2 - y*x^2 - 35;
+end
+
+function ret = dH(x,y)
+	%ret = 2*(x+6) + 8*x -2*x*y;
+	ret = 2*(y+6) -x^2;
+end
+
+function ret = Newton(H, dH, x, yi, lim)
+	% @H, @dH - the function and its derivative
+	% xi - initial guess for x
+	% y - the stationary y value
+	% lim - small height for convergence criteria
+	l = 10; %large initial value
+	n = 0;
+	while l > lim
+		yi = yi - H(x,yi)/dH(x, yi);
+		l = abs(H(x,yi));
+		n = n + 1;
+		if n > 50
+			ret = inf; % set a number that won't be plotted
+			break
+		end
+	end
+	ret = yi;
+end

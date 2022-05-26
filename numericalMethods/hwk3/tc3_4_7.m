3073d4fae598ac1516e7df61adf8d3569645810f
Tclack88 <tclack@aol.com>
2022-05-26 13:29:46 +1000

    added some workshope solutions

---
+++ numericalMethods/hwk3/tc3_4_7.m
@@ -0,0 +1,100 @@
+% Unsteady convection -- diffusion
+% dO/dt + U*dO/dx = v*D2O/Dx2
+% Use N=40 and dt = .1
+
+dt = .1
+a = 0; % left endpoint for both t and x
+b = 20;% right endpoint for both t and x
+t = [a:dt:b];
+N=40;
+N_1 = N-1; % N-1, GLL nodes gives one more point than the number passed in
+%x = linspace(0,20,N);
+x = GLL_nodes(N_1);
+x = (b-a)/2*x + (b+a)/2; % Adjust Gauss lobatto beyond the -1 +1 region
+D = DerivMatrix(x,N_1);
+
+O = zeros(length(t)-1,N);
+O(1,:) = initiate_phis(x);
+
+
+for n=1:length(t)-1
+	O(n+1,:) = O(n,:) + f(D, O(n,:))*dt;
+end;
+
+% plot phis for when t = 0,5,10,15
+times = [0 5 10 15];
+figure
+hold on
+for time = times
+	t_i = time/dt + 1; %index with our phis
+	plot(x,O(t_i,:))
+	xlabel('x')
+	ylabel('phi')
+	title('Phi at x for with dt=.1 and N=40 using spectral differentiation')
+end
+legend('time:'+string(times))
+hold off
+
+
+function vec = f(D,O)
+	% Return next set of phis
+	v = .025;
+	U = 1;
+	D2 = D*D;
+	% Return derivatives in vector:
+	L = v*D2 - U*D;
+	vec = L*O';
+	vec = vec';
+end
+
+
+function O = initiate_phis(x)
+	% pass in array of x,
+	% returns starting phis
+	O = exp(-((x-3)/.5).^2); % from BC
+	O(1) = 0; % from BC
+end
+
+function xp = GLL_nodes(N)
+        % provided in solutions workshop 8
+        syms x
+        Lo = (1-x^2)*diff(legendreP(N,x),x);
+        xp = double(vpasolve(Lo == 0));
+end
+
+function D = DerivMatrix(x,n)
+        % Spectral differentiation
+        % Find D matrix (given in lecture)
+        D = zeros(n+1,n+1);
+        for i = 1:n+1;
+                num(i)=1.0;
+                for k=1:n+1
+                        if k~=i
+                                num(i) = num(i)*(x(i)-x(k));
+                        end
+                end
+        end
+        for j=1:n+1;
+                den(j)=1.0;
+                for k =1:n+1
+                        if k~=j
+                                den(j) = den(j)*(x(j)-x(k));
+                        end
+                end
+        end
+        for i=1:n+1
+                for j=1:n+1
+                        if i~=j
+                                D(i,j)=num(i)/(den(j)*(x(i)-x(j)));
+                        end
+                end
+        end
+        for i=1:n+1
+                for k=1:n+1
+                        if i~=k
+                                D(i,i) = D(i,i) + 1./(x(i)-x(k));
+                        end
+                end
+        end
+end
+
--- numericalMethods/hwk3/tc3_5_2.m
+++ numericalMethods/hwk3/tc3_5_2.m
@@ -1,19 +1,37 @@
 %example from workshop 8.7
 
-n = 10;
+n = 40;
 x = linspace(-1,1,n)';
-err = [];
-for fi = 1:length(f_list);
-        y = f_list{fi}(x);
-        D = DerivMatrix(x,n-1);
-        dy = D*y;
-        dy_real = df_list{fi}(x);
-        e = rms(dy, dy_real);
-        err(end+1) = e;
-end
-average_error = sum(err)/length(err);
-disp('average rms error for n=10 points for each function')
-err
+y = linspace(-1,1,n)';
+% x = GLL_nodes(n-1);
+% y = GLL_nodes(n-1);
+[X,Y] = meshgrid(x,y);
+
+u = sin(pi*X).*cos(pi*Y);
+size(u)
+% numerical approximation to 2nd partial derivative
+D2 = SecondDerivMatrix(x,n);
+D2u = D2*u;
+I = eye(n);
+
+D2x = kron(I,D2);
+D2y = kron(D2,I);
+
+u=u';
+u = u(:);
+
+uxx = D2x*u;
+uyy = D2y*u;
+
+uxx = reshape(uxx,n,n);
+uyy = reshape(uyy,n,n);
+figure
+surf(uxx + uyy,X,Y);
+
+% compare to exact solution
+figure
+u_r = -2*pi^2*sin(pi*X).*cos(pi*Y);
+surf(u_r,X,Y);
 
 
 
@@ -52,4 +70,24 @@ function D = DerivMatrix(x,n)
                         end
                 end
         end
+end
+
+function D = SecondDerivMatrix(xp,N)
+        D = zeros(N);
+        D(:,1) = 1;
+        D(:,2) = -2;
+        D(:,3) = 1;
+        for i = 3:N-1;
+                D(i,:) = circshift(D(i,:),i-2);
+        end
+        D(end,:) = circshift(D(end,:),N-3);
+        delta = xp(2) - xp(1); % same as interval linspace(a,b,N+1)
+        D = D/(delta^2);
+end
+
+function xp = GLL_nodes(N)
+	% provided in solutions workshop 8
+	syms x
+	Lo = (1-x^2)*diff(legendreP(N,x),x);
+	xp = double(vpasolve(Lo == 0));
 end

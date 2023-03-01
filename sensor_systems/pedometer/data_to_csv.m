f15912581c72badc25d25bd3bd19cc819998b6fb
Tclack88 <tclack@aol.com>
2023-03-02 10:12:02 +1100

    add 100 steps data

--- sensor_systems/pedometer/data_to_csv.m
+++ sensor_systems/pedometer/data_to_csv.m
@@ -1,5 +1,6 @@
-filename = '200steps_sensorlog_20230228_113308.mat';
+%filename = '200steps_sensorlog_20230228_113308.mat';
+filename = '100step_sensorlog_20230302_100113.mat';
 raw_dat = load(filename);
 dat = struct2table(raw_dat)
 T = splitvars(dat)
-writetable(T ,'200steps.csv');
+writetable(T ,'100steps.csv');

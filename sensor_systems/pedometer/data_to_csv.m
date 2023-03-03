f9758b09d7d9114def971d8d0e28b9805a0b8b18
Tclack88 <tclack@aol.com>
2023-03-03 18:24:50 +1100

    converted background to csv instead of lame .mat file

--- sensor_systems/pedometer/data_to_csv.m
+++ sensor_systems/pedometer/data_to_csv.m
@@ -1,6 +1,8 @@
 %filename = '200steps_sensorlog_20230228_113308.mat';
 filename = '100step_sensorlog_20230302_100113.mat';
+filename = 'Background_sensorlog_20230303_180709.mat'
 raw_dat = load(filename);
 dat = struct2table(raw_dat)
 T = splitvars(dat)
-writetable(T ,'100steps.csv');
+writetable(T ,'background.csv');
+%writetable(T ,'100steps.csv');

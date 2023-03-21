5941568161b4f93fa438b3427bc103c594bc31e1
Tclack88 <tclack@aol.com>
2023-03-21 11:53:04 +1100

    updload more recent data for testing different paces

--- sensor_systems/pedometer/extract_matlab_data.m
+++ sensor_systems/pedometer/extract_matlab_data.m
@@ -1,10 +1,19 @@
-filename = '100step_sensorlog_20230302_100113.mat';
-filename = '250_pocket_pause_sensorlog_20230316_134817.mat';
+% filename = '100step_sensorlog_20230302_100113.mat';
+% filename = '250_pocket_pause_sensorlog_20230316_134817.mat';
 %filename = 'Background_sensorlog_20230307_110521.mat'
+
+filename = '/home/tclack/Downloads/100_run_pause_sensorlog_20230321_112444.mat'
+filename = '/home/tclack/Downloads/180_run_sensorlog_20230321_112548.mat'
+filename = '/home/tclack/Downloads/50_hand_slow_sensorlog_20230321_113224.mat'
+% filename = '/home/tclack/Downloads/50_mix_sensorlog_20230318_130007.mat'
+filename = '/home/tclack/Downloads/60_pocket_slow_sensorlog_20230321_112311.mat'
+ilename = '/home/tclack/Downloads/70_pause_demo_sensorlog_20230321_112801.mat'
+filename = '/home/tclack/Downloads/80_run_hand_sensorlog_20230321_114218.mat'
+
 raw_dat = load(filename);
 dat = struct2table(raw_dat);
 T = splitvars(dat);
-writetable(T ,'250_pocket_pause.csv');
+writetable(T ,'80_run_hand.csv');
 %writetable(T ,'100steps.csv');
 %writetable(T ,'background.csv');
 
